ddd123baed521f4603c357d3efffe1be8e128c68
Md. Arefin Islam <arefinshuvo24@gmail.com>
2026-09-02 20:25:39 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,11 @@
+const price1 = 30;
+const price2 = 50;
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 < price2);
+console.log(price1 >= price2);
+console.log(price1 <= price2);
+

8d26fe3e382c0a6cc5562037d32268941e4b52d5
Md. Arefin Islam <arefinshuvo24@gmail.com>
2026-09-03 03:35:09 +0600

    added

--- 18.price(ternary).js
+++ 18.price(ternary).js
@@ -1,2 +1,4 @@
+// If the price is greater than 1000, print "Expensive", otherwise "Affordable" — write this using a ternary operator.
+
 const price = 2000;
 console.log(price>=1000 ? "Expensive" : "Affordable");
